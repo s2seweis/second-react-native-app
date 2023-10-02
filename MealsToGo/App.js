510bffccdc4f05936648985681efb81d1958a46e
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-10-02 21:46:25 +0200

    geting used to react native flex & styling

--- MealsToGo/App.js
+++ MealsToGo/App.js
@@ -1,20 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
 
-export default function App() {
+export default function App () {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={styles.container}
+
+      // style={{
+      //   flex: 1,
+      //   width: 'auto',
+      //   height: 500,
+      //   backgroundColor: 'blue',
+      //   flexDirection: '',
+      // }}
+    >
+      <View
+        style={styles.box1}
+        // style={{
+        //   flex: 1,
+        //   width: 'auto',
+        //   height: 100,
+        //   backgroundColor: 'red',
+        // }}
+      />
+      <View
+        style={styles.box2}
+        // style={{
+        //   flex: 1,
+        //   width: 'auto',
+        //   height: 'auto',
+        //   flexGrow: 1,
+        //   backgroundColor: 'yellow',
+        // }}
+      >
+        <Text style={{color: 'black', margin: 50, fontSize: 30}}>
+          Just a test!!!
+        </Text>
+      </View>
+      <View
+        style={styles.box3}
+
+        // style={{
+        //   flex: 1,
+        //   width: 'auto',
+        //   height: 100,
+        //   backgroundColor: 'green',
+        // }}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create ({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: 'auto',
+    height: 500,
+    backgroundColor: 'blue',
+    flexDirection:"column"
+    // flexDirection:"row"
+  },
+  box1: {
+    flex: 1,
+    width: 'auto',
+    height: 100,
+    backgroundColor: 'red',
+    flexGrow:1
+  },
+  box2: {
+    flex: 1,
+    width: 'auto',
+    height: 100,
+    backgroundColor: 'yellow',
+    flexGrow: 1,
+  },
+  box3: {
+    flex: 1,
+    width: 'auto',
+    height: 100,
+    backgroundColor: 'green',
+    flexGrow: 1,
   },
 });
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # second-react-native-app
+- Title: MealsToGo
+- Practise Native Development
+
+# Features
+- Firebase Database
+- Authentication
+- Camera
+- Google Maps
+- Stripe Payment
