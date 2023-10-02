d7f57396b1f0cb0e7d56560f2925c18bb708b6ad
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-10-02 22:12:48 +0200

    upload

--- MealsToGo/App.js
+++ MealsToGo/App.js
@@ -1,84 +1,59 @@
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
 
 export default function App () {
   return (
-    <View
-      style={styles.container}
-
-      // style={{
-      //   flex: 1,
-      //   width: 'auto',
-      //   height: 500,
-      //   backgroundColor: 'blue',
-      //   flexDirection: '',
-      // }}
+    <SafeAreaView
+      style={{
+        flex: 1,
+        width: 'auto',
+        height: 500,
+        flexDirection: '',
+      }}
     >
+      {/* <View
+        style={{
+          // flex: 1,
+          height: 50,
+          backgroundColor: 'grey',
+          width: 'auto',
+        }}
+      /> */}
       <View
-        style={styles.box1}
-        // style={{
-        //   flex: 1,
-        //   width: 'auto',
-        //   height: 100,
-        //   backgroundColor: 'red',
-        // }}
-      />
-      <View
-        style={styles.box2}
-        // style={{
-        //   flex: 1,
-        //   width: 'auto',
-        //   height: 'auto',
-        //   flexGrow: 1,
-        //   backgroundColor: 'yellow',
-        // }}
+        style={{
+          // flex: 1,
+          height: 50,
+          backgroundColor: 'green',
+        }}
       >
-        <Text style={{color: 'black', margin: 50, fontSize: 30}}>
-          Just a test!!!
-        </Text>
+        <Text style={{padding: 10}}>Line:1</Text>
       </View>
       <View
-        style={styles.box3}
+        style={{
+          flex: 1,
+          height: 100,
+          flexGrow: 1,
+          backgroundColor: 'blue',
+        }}
+      >
+        <Text style={{padding: 10}}>Line:2</Text>
+      </View>
 
-        // style={{
-        //   flex: 1,
-        //   width: 'auto',
-        //   height: 100,
-        //   backgroundColor: 'green',
-        // }}
-      />
-    </View>
+      {/* <View style={{
+          // flex: 1,
+          height: 50,
+          backgroundColor:"grey"
+        }} /> */}
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create ({
   container: {
+    alignItems: 'center',
+    justifyContent: 'center',
     flex: 1,
-    width: 'auto',
-    height: 500,
-    backgroundColor: 'blue',
-    flexDirection:"column"
-    // flexDirection:"row"
-  },
-  box1: {
-    flex: 1,
-    width: 'auto',
-    height: 100,
-    backgroundColor: 'red',
-    flexGrow:1
-  },
-  box2: {
-    flex: 1,
-    width: 'auto',
-    height: 100,
-    backgroundColor: 'yellow',
-    flexGrow: 1,
-  },
-  box3: {
-    flex: 1,
-    width: 'auto',
-    height: 100,
-    backgroundColor: 'green',
-    flexGrow: 1,
+    backgroundColor: 'lightblue',
   },
 });
